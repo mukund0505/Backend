7acafe0a34f00ce3d9d4c39a05137cb1b08a8763
mukund <mukundchavan6904@gmail.com>
2024-03-20 10:39:33 +0530

    use postman for backend

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // How to get user details . Deatils can be came from either form,json(then we used req.body) or URL
   const { fullname, email, username, password } = req.body;
-  console.log("email: ", email);
+  // console.log("email: ", email);
 
   // we can individual check each userdeatils like below
   // if (fullname == "") {
@@ -31,17 +31,27 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
+  // console.log(req.files);
 
   // multer gives us access to files and middlewares gives access to body
-  const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -57,7 +67,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     fullname,
     avatar: avatar.url,
-    coverImage: coverImage.url || "",
+    coverImage: coverImage?.url || "",
     email,
     password,
     username: username.toLowerCase(),
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,16 +5,17 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]), 
-    registerUser);
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
